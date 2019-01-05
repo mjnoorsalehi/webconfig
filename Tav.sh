5abc714d5ef014c40a36fadb74a46e8d3d715c02
saeedm66 <mansouri97@yahoo.com>
2019-01-05 18:29:05 +0330

    first point

--- Tav.sh
+++ Tav.sh
@@ -1,10 +1,17 @@
-menu() {
+	menu() {
 	echo "1.creat file"
 	echo "2.list of file"
 	echo "3.exit"
 	read num
-}
-menu
+	}
+	menu
 while(($num!=3))
+	if(($inp=="1"))
+        then
+                echo "please enter file name:"
+                read filename
+                sudo touch $filename
+                ls -lah
+
 do
 done
